219ea2559990023117df5c89a87031319f098041
InValidFire <housdenlt@gmail.com>
2019-05-25 16:07:58 -0400

    Update system cross-platform compatibility - Should now be able to handle updates on both linux and windows systems, will find out shortly.

--- cogs/update.py
+++ cogs/update.py
@@ -2,6 +2,7 @@ from discord.ext import tasks, commands
 import discord
 import subprocess
 import os
+import sys
 
 class Update(commands.Cog):
     debug = True
@@ -13,16 +14,19 @@ class Update(commands.Cog):
     @commands.command()
     async def update(self,ctx):
         """ Updates the bot's code from the master branch """
-
-#        subprocess.run('update.py')
-#        subprocess.run('update.bat')
+        
         role = discord.utils.get(ctx.guild.roles, name="Lapis Lord")
         #look into the specifics of ctx works in the discord.py documentation later, appears to get the context of the command, but what does that include
         dir = os.getcwd()
         if(role in ctx.author.roles):
-            subprocess.run(['start','py',dir+'\\update.py'],shell=True)
-            await ctx.send("Rebooting for update!")
-            exit()
+            if(sys.platform == 'win32'): #handles updates on windows systems
+                subprocess.run(['start','py',dir+'\\update.py'],shell=True)
+                await ctx.send("W: Rebooting for an update!")
+                exit()
+            if(sys.platform == 'linux'): #handles updates on linux systems
+                subprocess.run(['x-terminal-emulator','-e','python3.7',dir+'\\update.py'],shell=True)
+                await ctx.send("L: Rebooting for an update!")
+                exit()
         await ctx.send("You do not have permission to use this command.")
 def setup(bot):
     bot.add_cog(Update(bot))
--- update.py
+++ update.py
@@ -3,13 +3,17 @@ import os
 import time
 
 print("Starting Update Script")
-subprocess.run('git checkout master',shell=True)
+subprocess.run('git checkout master',shell=True) #need to find a way to stop the script if git doesn't properly change branches, look into subprocess more.
 print("Changed to master branch")
 subprocess.run('git fetch',shell=True)
 print("Fetched code, now waiting 30 seconds before bot reboot")
 time.sleep(30)
 print("Rebooting bot")
 dir = os.getcwd()
-subprocess.run(['start','py',dir+'\\bot.py'],shell=True)
+if(sys.platform == 'win32'): #restarts bot on windows systems
+    subprocess.run(['start','py',dir+'\\update.py'],shell=True)
+if(sys.platform == 'linux'): #restarts bot on linux systems
+    subprocess.run(['x-terminal-emulator','-e','python3.7',dir+'\\update.py'],shell=True)
 print("Closing Update Script")
+time.sleep(3)
 exit()
