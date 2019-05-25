483230de8e2ea0862813db0068001302d2601340
InValidFire <housdenlt@gmail.com>
2019-05-25 15:40:40 -0400

    Improved update directory handling - Now should be cross-os compatible - Uses relative directory instead of an absolute system path.

--- cogs/update.py
+++ cogs/update.py
@@ -1,6 +1,7 @@
 from discord.ext import tasks, commands
 import discord
 import subprocess
+import os
 
 class Update(commands.Cog):
     debug = True
@@ -16,8 +17,10 @@ class Update(commands.Cog):
 #        subprocess.run('update.py')
 #        subprocess.run('update.bat')
         role = discord.utils.get(ctx.guild.roles, name="Lapis Lord")
+        #look into the specifics of ctx works in the discord.py documentation later, appears to get the context of the command, but what does that include
+        dir = os.getcwd()
         if(role in ctx.author.roles):
-            subprocess.run(['start','py','C:\\Users\\Valid\\OneDrive\\Desktop\\Lapis-Bot-Rewrite\\update.py'],shell=True)
+            subprocess.run(['start','py',dir+'\\update.py'],shell=True)
             await ctx.send("Rebooting for update!")
             exit()
         await ctx.send("You do not have permission to use this command.")
