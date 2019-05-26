b36ad9d576e1edc6f611a3069a91324235b6bc5d
InValidFire <housdenlt@gmail.com>
2019-05-26 00:51:27 -0400

    Syntax fix

--- cogs/update.py
+++ cogs/update.py
@@ -24,7 +24,7 @@ class Update(commands.Cog):
                 await ctx.send("W: Rebooting for an update!")
                 exit()
             if(sys.platform == 'linux'): #handles updates on linux systems
-                process = subprocess.Popen(['x-terminal-emulator','-e','python3.7 bot.py'],shell=True, stdout=PIPE)
+                process = subprocess.Popen(['x-terminal-emulator','-e','python3.7 bot.py'],shell=True, stdout=subproces.PIPE)
                 print(process.returncode)
                 await ctx.send("L: Rebooting for an update!")
                 exit()
