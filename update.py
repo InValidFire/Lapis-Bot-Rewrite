d3cff2607363cac78de4e683dcc3766656956be6
InValidFire <housdenlt@gmail.com>
2019-05-27 02:24:39 -0400

    Made branch.py and update.py force exit if successful - Last attempt didn't work, but this one looks promising.     - uses os._exit(0) now instead of exit() to avoid triggering the except block below it.

--- update.py
+++ update.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 import sys
 import shlex
+import os
 
 print("Starting Update Script")
 try:
@@ -10,23 +11,23 @@ try:
     subprocess.run('git pull',shell=True)
     print("Fetched code, making success note")
     successfile = open("update_success.temp","w+") #to be processed by the bot at boot if present.
-    successfule.close()
+    successfile.close()
     print("Rebooting bot")
     time.sleep(2)
     if(sys.platform == 'win32'): #restarts bot on windows systems
-        subprocess.run(['start','py',dir+'\\bot.py'],shell=True)
+        subprocess.run(['start','py','bot.py'],shell=True)
     if(sys.platform == 'linux'): #restarts bot on linux systems
         process = subprocess.Popen(shlex.split("""x-terminal-emulator -e python3.7 bot.py"""), stdout=subprocess.PIPE)
     print("Closing Update Script")
     time.sleep(3)
-    exit()
-except CalledProcessError:
+    os._exit(0) #forces an exit, avoids triggering the except block
+except:
     errorfile = open("update_error.temp","w+") #to be processed by the bot at boot if present.
     errorfile.close()
     print("Failed checkout, rebooting bot")
     time.sleep(2)
     if(sys.platform == 'win32'): #restarts bot on windows systems
-        subprocess.run(['start','py',dir+'\\bot.py'],shell=True)
+        subprocess.run(['start','py','bot.py'],shell=True)
     if(sys.platform == 'linux'): #restarts bot on linux systems
         process = subprocess.Popen(shlex.split("""x-terminal-emulator -e python3.7 bot.py"""), stdout=subprocess.PIPE)
     print("Closing Update Script")
