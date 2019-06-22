c11ec960cb0730d110f3dda01621fb2e2c1bb7da
InValidFire <wolvie2013@gmail.com>
2019-06-22 17:32:21 -0400

    Quick formatting fix - Wiki command works better when formatting spaces with a '_' rather than a '+'

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -13,7 +13,7 @@ class Wiki(commands.Cog):
     @commands.command(aliases=['search'])
     async def wiki(self,ctx,*args):
         """Search the Minecraft Wiki."""
-        with urlopen("https://minecraft.gamepedia.com/Special:Search/{}".format('+'.join(args))) as page:
+        with urlopen("https://minecraft.gamepedia.com/Special:Search/{}".format('_'.join(args))) as page:
             await ctx.send(str(page.geturl()))
 
 def setup(bot):
